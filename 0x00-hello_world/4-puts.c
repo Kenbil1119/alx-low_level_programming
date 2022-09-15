5f52d02b04a2b186cb647756343c4cd96344fa2d
Usman Saheed <uskehinde@gmail.com>
2022-09-15 22:34:23 +0100

    creating C code using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
+/**
+ * main - prints "\"Programming is like building a multilingual puzzle"
+ * main - function "puts"
+ * Return: Always 0.
+*/
 #include <stdio.h>
 
-/**
- * main - prints "Programming is like building a multilingual 
- * puzzle,follow by a new line."
- *
- * Return: Always 0.
- */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("\"Programming is like building a multilingual puzzle""\n");
 return (0);
 }
-
