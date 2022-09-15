7c80d480ee9eb900d04cba16b7ebfe837babba86
Usman Saheed <uskehinde@gmail.com>
2022-09-15 22:17:12 +0100

    creating a C code with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 /**
  * 5-printf.c:creating a code using funtion "printf" as the output funtion
  * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * main - function "printf"
  * Return: Always 0.
 */
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
