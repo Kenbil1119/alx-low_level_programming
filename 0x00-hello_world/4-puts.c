b6abcedf0f326e7aa18bd53e992f488cd971db79
Usman Saheed <uskehinde@gmail.com>
2022-09-15 19:43:50 +0100

    creating first file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 /*
  * This is the first C program task for me in ALX School.
- * the tasks ("Programming is like building a multilingual puzzle)
+ * main - prints "Programming is like building a multilingual puzzle
  * follow by a new line character
  * I was to use the function 'puts' not 'printf'
  * and ends with the value 0
@@ -10,7 +10,7 @@
 int main(void)
 
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 		return(0);
 }
