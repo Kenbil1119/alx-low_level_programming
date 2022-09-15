882a26e6997077252786ff09d1b58d86a16dda5d
Usman Saheed <uskehinde@gmail.com>
2022-09-15 18:56:03 +0100

    writing the first code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * This is the first C program task for me in ALX School.
+ * the tasks ("Programming is like building a multilingual puzzle)
+ * follow by a new line character
+ * I was to use the function 'puts' not 'printf'
+ * and ends with the value 0
+ */
+#include "stdio.h"
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+		return(0);
+}
+
