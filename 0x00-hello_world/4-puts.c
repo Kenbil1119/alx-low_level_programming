be42ae2d9d0417ab7e97862b69e6eea9ec1b1b9d
Usman Saheed <uskehinde@gmail.com>
2022-09-15 20:04:58 +0100

    creating a C program in a c file using betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle
- * follow by a new line character
- * I was to use the function 'puts' not 'printf'
- * and ends with the value 0
+ * main - prints "Programming is like building a multilingual 
+ * puzzle,follow by a new line."
+ *
+ * Return: Always 0.
  */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 
