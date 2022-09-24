2cfda38732687199060bff0c6135be998eeda0bc
Usman Saheed <uskehinde@gmail.com>
2022-09-24 09:47:41 +0100

    the last digit of n numbers if 0, < 6, > 5.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - function: "printf"
  * main - prints The last digit of the number stored in the
- * 	variable n.
+ * variable n.
  *
  * Return: Always (0)
  */
@@ -18,15 +18,15 @@ int main(void)
 
 	if ((n % 10) > 5)
 {
-	printf("Last digit of n %d is %d and is greater than 5\n", n, n % 10);
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 	else if ((n % 10) < 6 && (n % 10) != 0)
 {
-	printf("Last digit of n %d is %d and is less than 6 and not 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 	else
 {
-	printf("Last digit of n %d is %d and is 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 return (0);
 }
