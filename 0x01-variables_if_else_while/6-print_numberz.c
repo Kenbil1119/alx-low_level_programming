f66a06ab16ed3d08721f2ba2d344f80c8ef097c9
Usman Saheed <uskehinde@gmail.com>
2022-09-24 13:13:23 +0100

    printing all single digit number in base 10 using putchar without any variables

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,8 @@
 
 /**
  * main - function "putchar"
- * main - prints all single digit numbers in base 10 starting from 0 using putchar, without using any char variables.
+ * main - prints all single digit numbers in base 10 starti
+ * ng from 0 using putchar, without using any char variables.
  *
  * Return: Always (0)
  */
@@ -11,6 +12,7 @@
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	putchar('\n');
