aba9442cae038711e4598d1f681ef2682f804505
Usman Saheed <uskehinde@gmail.com>
2022-09-25 13:29:35 +0100

    print single combinations of a number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function "putchar"
+ * main - prints all posible combinations of single-digit number
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int n = 15;
+
+	while (n < 38)
+	{
+		putchar(n);
+		if (n != 38)
+		{
+			putchar(32);
+			putchar(34);
+		}
+		n++;
+	}
+	putchar(10);
+return (0);
+}
+			
