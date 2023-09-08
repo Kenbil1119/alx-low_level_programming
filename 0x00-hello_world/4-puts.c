7b325d1c51a3884c3fc473130cd681cd4cf696c4
Usman Saheed <uskehinde@gmail.com>
2023-09-08 14:01:22 -0700

    Printing to the standard output from C source using \puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: puts
+ *
+ * Conditional Statement:
+ * Loop Statement:
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
