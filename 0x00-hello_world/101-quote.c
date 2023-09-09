7d08b43feee9239dbba33ed2fd1544568e9f3a84
Usman Saheed <uskehinde@gmail.com>
2023-09-08 17:11:37 -0700

    Printing to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,10 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 
 	write(2, str, sizeof(str));
 
