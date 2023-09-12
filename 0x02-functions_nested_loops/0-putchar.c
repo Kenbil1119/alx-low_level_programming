465946d70972a9946adfbc812eef0980630b0625
Usman Saheed <uskehinde@gmail.com>
2023-09-12 00:47:13 -0700

    Printing to the standard output

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * main - Function: printf
+ *
+ * main - prints "_putchar" to the standard output
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This repository contains C files which shows some examples on the concepts of Loop and loop nesting in C
