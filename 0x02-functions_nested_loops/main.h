94568d3b5e86d8e1e3de54afc215ab32162ea80f
Usman Saheed <uskehinde@gmail.com>
2023-09-13 11:32:21 -0700

    Define print_putchar()

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,16 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 
 #endif
+
+/**
+ * print_putchar - To print "_putchar"
+ *
+ * function: printf
+ *
+ * Return:
+ */
+
+void print_putchar(void)
+{
+	printf("_putchar");
+}
