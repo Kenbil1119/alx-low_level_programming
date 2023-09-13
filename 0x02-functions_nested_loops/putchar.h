49ef87760de552c44a03a02ea7b9e4872c684dba
Usman Saheed <uskehinde@gmail.com>
2023-09-13 14:08:16 -0700

    Calling print_putchar()  in main()

---
+++ 0x02-functions_nested_loops/putchar.h
@@ -0,0 +1,19 @@
+#ifndef PUTCHAR_H
+#define PUTCHAR_H
+
+#include <stdio.h>
+
+/**
+ * print_putchar - To print "_putchar"
+ *
+ * function: printf
+ *
+ * Return:
+ */
+
+void print_putchar(void)
+{
+	printf("_putchar");
+}
+
+#endif
