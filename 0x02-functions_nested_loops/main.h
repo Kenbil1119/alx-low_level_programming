2a1bcd2f7d26cc771b3a0ab1ba7df477083dfa1d
Usman Saheed <uskehinde@gmail.com>
2023-09-13 13:49:29 -0700

    Printing lowercase alphabet by calling print_alphabet() in main() from the main.h header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-void print_putchar(void);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-
 /**
  * print_putchar - To print "_putchar"
  *
