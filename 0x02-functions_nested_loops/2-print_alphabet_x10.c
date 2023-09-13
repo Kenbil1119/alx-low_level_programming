9106be3cf49906db1c801c417f8bf2ed62b27e81
Usman Saheed <uskehinde@gmail.com>
2023-09-13 01:41:25 -0700

    Using function in function to prints lowercase alphabet 10 times using putchar()

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,31 @@
 #include "main.h"
 
 /**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Function: putchar()
+ * Data Type: int, char
+ *
+ * Loop Statement: for
+ *
+ * Return Type: void
+ * Return:
+ */
+
+void print_alphabet_x10(void)
+{
+	int n_times;
+	char alpha;
+	
+	for (n_times = 0; n_times <= 9; n_times++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			putchar(alpha);
+		putchar(10);
+	}
+}
+
+/**
  * main - Entry point
  *
  * main - Function: print_alphabet_x10()
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef stdio
-#define stdio
+#ifdef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 void print_putchar(void);
@@ -7,73 +7,3 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 
 #endif
-
-/**
- * print_putchar - Print "_putchar"
- *
- * Function: printf
- *
- * Parameter: void
- *
- * print_putchar - prints: print "_putchar" to the standard output
- *
- * Return - type: void
- * Return:
- */
-
-void print_putchar(void)
-{
-	printf("_putchar");
-}
-
-/**
- * print_alphabet - print alphabet "a - z"
- *
- * Function: putchar
- * Data - type: char
- *
- * Loop Statement: for
- *
- * print_alphabet: prints all lowercase characters of alphabet
- *
- * Return - type: void
- * Return:
- */
-
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-		putchar(alpha);
-	putchar(10);
-}
-
-/**
- * print_alphabet_x10 - Alphabet 10 times
- *
- * Function: putchar()
- * Data - type: int, char
- *
- * Loop Statement: for
- *
- * print_alpha_x10 - Call putchar to prints lowercase alphabet
- *		10 times to the standard out and put newline at
- *		the end of each call
- *
- * Return - type: void
- * Return:
- */
-
-void print_alphabet_x10(void)
-{
-	char alpha;
-	int n_times;
-
-	for (n_times = 0; n_times <= 9; n_times++)
-	{
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
-			putchar(alpha);
-		putchar(10);
-	}
-}
