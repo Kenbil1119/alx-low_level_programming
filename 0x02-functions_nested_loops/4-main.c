30bef30a13c2cc7066185b312e339832a3f425be
Usman Saheed <uskehinde@gmail.com>
2023-09-16 04:55:48 -0700

    Checking if a character is an alphabet character using isalpha()

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Function: _isalpha
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_isalpha();
+
+	return (0);
+}
