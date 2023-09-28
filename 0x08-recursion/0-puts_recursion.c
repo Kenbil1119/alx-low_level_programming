b20d8dd78adcb5b36aab415ea7a94ebda7207a27
Usman Saheed <uskehinde@gmail.com>
2023-09-28 14:02:32 -0700

    Printing string to the standard output using puts() recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Invoke puts()
+ *
+ * @s: Character parameter to get string
+ *
+ * Function: puts()
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/0-test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Invoke _puts_recursion()
+ *
+ * Function: _puts_recursion()
+ *
+ * main: Print string to the standard output with a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+
+	return (0);
+}
