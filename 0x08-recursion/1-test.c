3c31b338c5e69b7543d3e093c88a9d8c44b5495f
Usman Saheed <uskehinde@gmail.com>
2023-09-29 11:06:36 -0700

    Printing string in reverse with recursion

---
+++ 0x08-recursion/1-test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	_print_rev_recursion("School");
+	_putchar(10);
+
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,5 +3,6 @@
 
 void _putchar(char ch);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif
