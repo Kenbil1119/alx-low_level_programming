07ab75ded05ebf2120f5226cf1eba9f265a39e6d
Usman Saheed <uskehinde@gmail.com>
2023-09-29 12:44:08 -0700

    Print string in reverse using recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /**
- * _print_rev_return - Print reverse of a string
+ * _print_rev_recursion - Print reverse of a string
  *
  * Function: _putchar(), _print_rev_recursion
- * 
+ *
  * @s: Character parameter to get string
  *
- * Conditional Statement - if-else: Check if string get string terminator else keep invoking _print_rev_recurtion()
+ * Conditional Statement - if: Check if string get string
+ *	terminator else keep invoking _print_rev_recurtion()
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	else
-		_print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
