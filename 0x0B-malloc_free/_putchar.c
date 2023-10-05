8be63d2368c85a845fb28c15a501294f01603b31
Usman Saheed <uskehinde@gmail.com>
2023-10-05 10:53:09 -0700

    Creating an array with a string literal using malloc

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _putchar - Invoke putchar()
+ *
+ * @c: Character parameter to be print to the standard output
+ *
+ * Function - putchar(): Call to print 'c'
+ */
+
+void _putchar(char c)
+{
+	putchar(c);
+}
