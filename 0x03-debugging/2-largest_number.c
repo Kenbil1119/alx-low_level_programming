dbbc70bc4072305a0ca146307c500c859f84e6da
Usman Saheed <uskehinde@gmail.com>
2023-10-15 14:49:34 -0700

    Debugging a program manually

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - Checks the largest number from three integers
+ *
+ * @a: First integer
+ * @b: Second integer
+ * @c: Third integer
+ *
+ * Return: Largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	/*if (a  > b && b > c)*/
+	if ((a > b) && (a > c))
+		largest = a;
+	/*else if (b > a && a > c)*/
+	else
+		if ((b > a) && (b > c))
+			largest = b;
+		else
+			largest = c;
+	return (largest);
+}
+
