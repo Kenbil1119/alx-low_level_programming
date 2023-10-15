c23ea16210dc37177ac601ad9302d7defb90ddc6
Usman Saheed <uskehinde@gmail.com>
2023-10-14 19:55:17 -0700

    Debugging of a file test C

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+# C Program Debugging Test
+
+Welcome! to 0x03-debugging directory.
+
+This directory is created to showcase Debugging in C program using a custom program (0-positive_or_negative.c) from 0x01-variables_if_else_while direcory
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * charge - check the charge on an integer data
+ *
+ * @i: Integer data to check
+ *
+ * Conditional Statement - if: Check if 'i' is not 0
+ *			- if-else: Check if 'i' is +ve or -ve
+ *
+ * charge: Print the respective charge of 'i'
+ */
+
+void  positive_or_negative(int i)
+{
+	int n;
+
+	n = i - i;
+
+	if ((i == 0) & (n == 0))
+		printf("Is Neutral\n");
+	else
+		if (i < 0)
+			printf("Is Negative\n");
+		else
+			printf("Is Positve\n");
+}
