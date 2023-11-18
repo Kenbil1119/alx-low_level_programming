72d9ac6b961f053d5cdd1ae12ac9674d61ca93dd
Usman Saheed .K <uskehinde@gmail.com>
2023-11-18 18:50:28 +0000

    White space remove to silent betty cheaker warning

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -7,18 +7,19 @@
  *			is not empty/terminated
  *
  * Return: 0
+ */
 
 int main(void)
 {
 	int i = 0;
 
 	char test[11] = "School Boy";
-	
+
 	printf("%s = ", test);
 
 	while (test[i])
 		printf("%i", _isupper(test[i++]));
 	putchar(10);
-	
+
 	return (0);
 }
