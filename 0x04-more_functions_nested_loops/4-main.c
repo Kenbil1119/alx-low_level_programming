87515e9546266c12d2367d632881ae5212be9174
Usman Saheed .K <uskehinde@gmail.com>
2023-11-19 08:38:44 +0000

    Print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Invoke print_most_umber to prints numbers
+ *		to the standard ouptut except 2 and 4
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_most_numbers();
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <string.h>
 
+void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
 int _putchar(char c);
