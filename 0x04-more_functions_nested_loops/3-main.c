ac74e494f84dbc04d4a53f06af3f11bb5f5480c7
Usman Saheed .K <uskehinde@gmail.com>
2023-11-19 08:26:40 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Invoke print_number to prints numbers
+ *		to the standard ouptut
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_numbers();
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _putchar - Print a characterto the standard output
+ *
+ * @c: Paramerter to read character
+ *
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	putchar(c);
+
+	return (0);
+}
