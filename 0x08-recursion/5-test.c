a390214f3b64ea3b65018149a20764b02cf960da
Usman Saheed .K <uskehinde@gmail.com>
2023-11-20 23:23:19 +0000

    Square root of a number using recursion

--- 0x08-recursion/5-test.c
+++ 0x08-recursion/5-test.c
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-	printf("Square root of 1 & 0 is: %d & %d\n", _sqrt(1), _sqrt(0));
+	puts("Square root of:");
+
+	printf("1 & 0 is: %d & %d\n", _sqrt_recursion(1), _sqrt_recursion(0));
+	printf("9 & 25 is: %d & %d\n", _sqrt_recursion(9), _sqrt_recursion(25));
+	printf("-1 & -9 is: %d & %d\n", _sqrt_recursion(-1), _sqrt_recursion(-9));
 
 	return (0);
 }
