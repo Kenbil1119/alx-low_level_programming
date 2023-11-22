6777c234abdee753470cd9de1528d176576017cc
Usman Saheed .K <uskehinde@gmail.com>
2023-11-22 14:52:52 +0000

    First test of command line arguments program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Program to test the usage of command line
+ *			arguments in C
+ *
+ * @argc: Integer Parameter to count total number of
+ *		arguments pass to the program
+ * @argv: Integer pointer parameter of array
+ *	pointing to each arguments pass to the program
+ *	on the command line and save it as an element
+ *	of array.
+ *
+ * Return: 0
+ *
+ * Author: Usman Saheed .K
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
