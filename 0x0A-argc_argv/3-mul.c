adecc17567bc85afd84446d2645ffcdf51cb0015
Usman Saheed .K <uskehinde@gmail.com>
2023-11-23 12:26:56 +0000

    Multiplying arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Program takes argument(s) from the command
+ *	line carried out multiplication of the
+ *	arguments after converting from string
+ *	to integer.
+ *
+ * @argc: Parameter for argument count
+ * @argv: Parameter for array of string for saving
+ *			argument as vector/element.
+ *
+ * Return: 1, if argc < 2
+ *		0 if more than 1 argument was passed
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, convert, result = 1;
+
+	if ((argc == 1) && argv[0])
+	{
+		printf("Error\n");
+
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		convert = atoi(argv[i]);
+		result *= convert;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void _putchar(char ch);
 
