0f999e95986d3a3ce2568cc6888af9e1ccd6a042
Usman Saheed .K <uskehinde@gmail.com>
2023-11-30 20:27:28 +0000

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,9 +16,14 @@
 
 int print_last_digit(int num)
 {
-	int last_digit = num % 10;
+	int last_digit;
 
-	printf("%d", last_digit);
+	if (num < 0)
+		num *= -1;
+
+	last_digit = num % 10;
+
+	_putchar(last_digit + 48);
 
 	return (last_digit);
 }
