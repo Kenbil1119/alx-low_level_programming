d857c0578503bdfbb34a39f491ffef520ebf4245
Usman Saheed .K <uskehinde@gmail.com>
2023-12-02 01:50:24 +0000

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sum_them_all - Read all pass argument and
+ *		return their some
+ *
+ * @n: Constant integer parameter to tell total number of
+ *			argument to be call for
+ * @...: Elipse sign, C syntax to allows function to take
+ *			many argument
+ *
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum, i = n;
+	va_list arg;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(arg, n);
+
+	while (i-- > 0)
+		sum += va_arg(arg, int);
+	va_end(arg);
+
+	return (sum);
+}
