7c72f7df57cc0ab641cb6a5d301bf01a50f87b83
Usman Saheed .K <uskehinde@gmail.com>
2023-12-02 06:43:55 +0000

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - Read all pass argument and
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTUONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdarg.h>
