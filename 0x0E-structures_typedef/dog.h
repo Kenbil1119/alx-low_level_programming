77390f446d28d02a1221202a520660b8333aa455
Usman Saheed .K <uskehinde@gmail.com>
2023-12-10 15:33:44 +0000

    Structure Data type example

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Data structure type with three variable
+ *
+ * @name: String constant of char* data type
+ * @age: Float data type
+ * @owner: string constant of char* data type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
