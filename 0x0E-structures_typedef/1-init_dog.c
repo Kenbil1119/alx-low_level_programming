ec24c91c24965a53898d7fe28004829ff8ce108c
Usman Saheed .K <uskehinde@gmail.com>
2023-12-13 06:52:37 +0000

    Initialize struct data

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -22,10 +22,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		exit(EXIT_FAILURE);
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
