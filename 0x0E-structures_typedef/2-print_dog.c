6715a007b740a79d452f94cae01b3e6d7fbbcf1d
Usman Saheed .K <uskehinde@gmail.com>
2023-12-13 07:36:48 +0000

    Print all element of dog structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Program print the elements of a struct data
+ *
+ * @d: A pointer pointing to a struct data type
+ *
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: %p\n", NULL);
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == 0.0)
+		printf("%p\n", NULL);
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("%p\n", NULL);
+	else
+		printf("Owner: %s\n", d->owner);
+}
