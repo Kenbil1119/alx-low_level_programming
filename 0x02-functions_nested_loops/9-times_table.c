050fafaa98b27bb6f4ce6684328c252ce033e04d
Usman Saheed .K <uskehinde@gmail.com>
2024-02-19 09:29:58 +0000

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,42 +4,15 @@
  * times_table - Function to print 9 times table
  */
 
-/*
-int _last_digit(int num);
-int _first_digit(int num);
-void _separator(void);
-*/
-
 void times_table(void)
 {
 	int i, j, n;
-	/*int ld = 0, fd = 0;*/
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
-			/*
-			if (n > 9)
-			{
-				fd = _first_digit(n);
-				ld = _last_digit(n);
-				_putchar(fd + 48);
-				_putchar(ld + 48);
-			}
-			else
-				_putchar(n + 48);
-			if (j != 9)
-			{
-				if (n > 9)
-					_separator();
-				else
-				{
-					_separator();
-					_putchar(' ');
-				}
-			}*/
 			if (j == 9)
 				printf("%2d\n", n);
 			else
@@ -50,19 +23,3 @@ void times_table(void)
 		}
 	}
 }
-/*
-int _last_digit(int num)
-{
-	return(num % 10);
-}
-
-int _first_digit(int num)
-{
-	return(num / 10);
-}
-
-void _separator(void)
-{
-	_putchar(',');
-	_putchar(' ');
-}*/
