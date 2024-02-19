6552ab44a13fe1c2d9c0c8053f2103a7e421c99c
Usman Saheed .K <uskehinde@gmail.com>
2024-02-19 10:15:39 +0000

    Sum of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,30 +1,15 @@
 #include "main.h"
 
 /**
- * add - Add two numbers and convert their sum from
- *	Integer to Character before printing out
+ * add - Add two numbers and print the sum
  *
  * @a: First Integer number
  * @b: Second Integer number
- *
- * sprintf(): Carried out the convertion from Integer
- *		to String if greater than 9 (Two digit)
  */
 
-void add(int a, int b)
+int add(int a, int b)
 {
-	int sum = a + b;
+	printf("%d\n", a + b);
 
-	if (sum > 9)
-	{
-		char str[98];
-		int i;
-
-		sprintf(str, "%d", sum);
-
-		for (i = 0; str[i] != '\0'; i++)
-			_putchar(str[i]);
-	}
-	else
-		_putchar(sum + 48);
+	return(a + b);
 }
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -5,8 +5,6 @@ int main(void)
 	int a = 10;
 	int b = 54;
 
-	printf("%d\n", a + b);
-
 	add(a, b);
 
 	return(0);
