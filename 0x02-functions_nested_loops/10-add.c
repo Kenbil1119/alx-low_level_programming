c20ae3ec57bee810cc261addf3092c4928f32e95
Usman Saheed .K <uskehinde@gmail.com>
2024-02-19 10:25:41 +0000

    Sum of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -13,5 +13,5 @@ int add(int a, int b)
 {
 	int sum = a + b;
 
-	return(sum);
+	return (sum);
 }
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * main - Call the addition function 'add()' which returns the
+ *			sum of two integer numbers
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	int a = 10;
@@ -9,5 +16,5 @@ int main(void)
 	sum = add(a, b);
 	printf("%d\n", sum);
 
-	return(0);
+	return (0);
 }
