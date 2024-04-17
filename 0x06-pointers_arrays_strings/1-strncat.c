f6a1fc2ff0c3d0ebecd4a25a63f3c42497a799a2
Usman Saheed .K <uskehinde@gmail.com>
2024-04-17 23:47:06 +0000

    N size concatenation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncat - Copy the string pointed by 'src' to 'dest'
+ *
+ * @dest: Pointer pointing to an old string.
+ *	it will be used to hold string and concatinate
+ *	string from 'src'
+ * @src: Pointer pointing to string to copy
+ * @n: Determine the n bytes of storgae in 'src' to copy
+ *
+ * Return: New string in 'dest'
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, dest_size = 0;
+	char *copy_str = dest;
+
+	dest_size = strlen(dest);
+
+	if (n == 0)
+		return (dest);
+
+	if (src == NULL)
+		return (dest);
+
+	for (i = 0, j = dest_size; i < n; i++, j++)
+		copy_str[j] = src[i];
+
+	return (copy_str);
+}
---
+++ 0x06-pointers_arrays_strings/1-test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print and copy N strings from 'str2' to 'str1'
+ *
+ * Function: printf(), putchar(), puts(), _strcat()
+ *
+ * NOTE: The string variable to append to must have enough space
+ *		to save the new appending string.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str1[50] = "Hello, World! ";
+	char str2[] = "ALX School is so cool!";
+
+	printf("str1: %s\n", str1);
+	printf("str2: %s\n", str2);
+
+	_putchar(10);
+
+	_puts("After calling '_strncat()'\n");
+
+	_strncat(str1, str2, 10);
+
+	printf("str1: %s\n", str1);
+	printf("str2: %s\n", str2);
+
+	return (0);
+}
