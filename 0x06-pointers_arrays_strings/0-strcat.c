9e55d9f89f105834447b3bf40b76c1d238383ffc
Usman Saheed .K <uskehinde@gmail.com>
2024-04-17 23:31:50 +0000

    string concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-#include "main.h"
 
 /**
- * _strcat - Calling strcat()
+ * _strcat - Copy the string pointed by 'src' to 'dest'
  *
- * Function: strcat()
+ * @dest: Pointer pointing to an old string.
+ *	it will be used to hold string and concatinate
+ *	string from 'src'
+ * @src: Pointer pointing to string to copy
  *
- * @dest: character parameter to append to
- * @src: character parameter to append from
- *
- * _strcat: Copy strings pointed by 'str' to the
- *		end of string pointed by 'dest'
- *
- * Return: dest
+ * Return: New string in 'dest'
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	unsigned int i, j, src_size = 0, dest_size = 0;
+	char *copy_str = dest;
 
-	return (dest);
+	src_size  = strlen(src);
+	dest_size = strlen(dest);
+
+	for (i = 0, j = dest_size; i < src_size; i++, j++)
+		copy_str[j] = src[i];
+
+	return (copy_str);
 }
