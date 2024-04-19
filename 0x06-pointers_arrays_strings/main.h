97c3b718005cd3431eb0e5c0edf70ae566a7c796
Usman Saheed .K <uskehinde@gmail.com>
2024-04-19 08:50:56 +0000

    Update Header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 void _putchar(char ch);
 void _puts(char *str);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+void print_array(int *a, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
 
 #endif
