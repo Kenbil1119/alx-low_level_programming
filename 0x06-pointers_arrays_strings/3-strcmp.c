2238bac9be5bfcc0172616c9a80151926a4b0ed5
Usman Saheed .K <uskehinde@gmail.com>
2024-04-21 21:46:24 +0000

    String Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,15 +2,21 @@
 
 
 /**
+ * _strcmp - Compare two string and returns
  *
+ * @s1: pointing to frist String
+ * @s2: Pointing to second String
+ *
+ * Return: The sum of the first character of the
+ *	first string and the first character of
+ *	the second string
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, cmp = 0;
+	int  cmp = 0;
 
-	for (i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
-		cmp += (s1[i] - s2[i]);
+	cmp = s1[0] - s2[0];
 
 	return (cmp);
 }
