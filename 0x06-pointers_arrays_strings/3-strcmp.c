65a486ad805d0bd075e230138ec859c973907bdf
Usman Saheed .K <uskehinde@gmail.com>
2024-04-22 11:21:41 +0000

    String Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,21 @@
  * @s1: pointing to frist String
  * @s2: Pointing to second String
  *
- * Return: The sum of the first character of the
- *	first string and the first character of
- *	the second string
+ * Return: The different value of the first character to
+ *	be different in both 's1' and 's2'.
+ *	Return 0 if no different found.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int  cmp = 0;
+	int i;
 
-	cmp = s1[0] - s2[0];
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		continue;
+	}
 
-	return (cmp);
+	return (0);
 }
