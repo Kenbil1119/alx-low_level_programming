574c139c12cea16bc6884db604ec3cf5df7c60c0
Usman Saheed .K <uskehinde@gmail.com>
2024-06-12 05:33:46 +0000

    Print n element of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,18 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if ((sizeof / int*) == 0)
+	{
+		printf('\n');
+		return;
+	}
+
+	if (n < 0)
+	{
+		printf('\n');
+		return;
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
