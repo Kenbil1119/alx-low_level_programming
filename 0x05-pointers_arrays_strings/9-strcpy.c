4c542e28c5a8cb17e1f663681605fbaa5ca1b2c8
Usman Saheed .K <uskehinde@gmail.com>
2024-06-12 06:25:47 +0000

    String copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,8 +5,12 @@
  *	to by src, including the terminating null byte
  *	(\0), to the buffer pointed to by dest.
  *
+ * @dest: Pointer to Buffer receiving copy
+ * @src: Pointer to string source
+ *
  * Return: the pointer to dest
- * FYI: The standard library provides a similar function: strcpy. Run man strcpy to learn more.
+ * FYI: The standard library provides a similar function:
+ *	strcpy. Run man strcpy to learn more.
  */
 
 char *_strcpy(char *dest, char *src)
@@ -14,11 +18,11 @@ char *_strcpy(char *dest, char *src)
 	unsigned long i, j;
 
 	if (src == NULL)
-		return dest;
+		return (dest);
 
-	for(i = 0, j = strlen(dest); src[i] != '\0'; i++, j++)
+	for (i = 0, j = strlen(dest); src[i] != '\0'; i++, j++)
 		dest[j] = src[i];
 	dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
