2c26f720988ac97fd4722efdde0ac9a199b899b4
Usman Saheed .K <uskehinde@gmail.com>
2024-06-12 04:27:30 +0000

    Half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,9 +14,11 @@ void puts_half(char *str)
 {
 	unsigned int n, i, len = strlen(str);
 
-	n = len / 2;
+	n = (len - 1) / 2;
 
 	for (i = n; i < len; i++)
+	{
 		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
