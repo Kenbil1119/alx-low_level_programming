4ba6556699cfe68490ab6514341e0beeb90f6520
Usman Saheed <uskehinde@gmail.com>
2025-04-28 21:54:50 +0100

    Custom String copy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -19,13 +19,14 @@ char *_strcpy(char *dest, char *src)
 
 	if (src == NULL)
 		return (dest);
+	 
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	dest[i + 1] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
